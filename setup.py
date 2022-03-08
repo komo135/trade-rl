70084fcd6359997885344d72c48788e5434e9ec9
komo <66017773+komo135@users.noreply.github.com>
2022-03-08 21:34:45 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,35 @@
 from setuptools import setup
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# long_description(後述)に、GitHub用のREADME.mdを指定
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name='traderl',
+    packages=['traderl.agent', 'traderl.agent'],
+
     version='1.0.0',
-    packages=['traderl.agent', 'traderl.nn'],
-    url='',
-    license='MIt',
-    author='nagi arai',
+
+    license='MIT',
+
+    install_requires=['numpy', 'tensorflow', "ta", "pandas"],
+
+    author='komo135',
     author_email='komoootv@gmail.com',
-    description=''
+
+    url='https://github.com/komo135/',  # パッケージに関連するサイトのURL(GitHubなど)
+
+    description='Reinforcement learning is used to learn trades.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    keywords='traderl',
+
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.7',
+    ],
 )
