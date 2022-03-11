795e325448e9b4e98dfcc3a6002dff038be1b92e
komo <66017773+komo135@users.noreply.github.com>
2022-03-11 13:55:02 +0900

    Add files via upload

---
+++ traderl/data/get_data.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import pandas_datareader as pdr
+
+
+def get_forex_data(symbol: str, timeframe: str):
+    """
+    :param symbol: AUDJPY, AUDUSD, EURCHF, EURGBP, EURJPY, EURUSD, GBPJPY, GBPUSD, USDCAD, USDCHF, USDJPY, XAUUSD
+    :param timeframe: m15, m30, h1, h4, d1
+    :return: pandas DataFrame
+    """
+
+    url = "https://raw.githubusercontent.com/komo135/forex-historical-data/main/"
+    url += symbol + "/" + symbol + timeframe.lower() + ".csv"
+
+    return pd.read_csv(url)
+
+
+def get_stock_data(symbol, start=None, end=None):
+    return pdr.get_data_yahoo(symbol, strt=start, end=end)
+
+
+__all__ = ["get_stock_data", "get_forex_data"]
